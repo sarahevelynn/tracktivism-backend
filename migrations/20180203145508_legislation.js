f4372b0c18e7c78d7f5cc1483b2845b77fc14770
SarahEvelynn Johnson <sarah@5280startupsolutions.co>
2018-06-14 14:33:53 -0600

    fixed author settings

--- migrations/20180203145508_legislation.js
+++ migrations/20180203145508_legislation.js
@@ -8,4 +8,6 @@ exports.up = function(knex, Promise) {
   });
 };
 
-exports.down = function(knex, Promise) {  return knex.schema.dropTableIfExists("legislation");};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("legislation");
+};
--- migrations/20180214121907_catagories.js
+++ migrations/20180214121907_catagories.js
@@ -1,9 +1,8 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("catagories", table => {
     table.increments("id").primary();
     table.text("KeyWords");
-});
+  });
 };
 
 exports.down = function(knex, Promise) {
